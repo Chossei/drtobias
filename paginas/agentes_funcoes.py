b56879bf32ebb264d1945a80dc0d0fdec357212b
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-08-19 15:07:15 -0300

    Corrigi a função relator. Precisava passar como buffer de bytes para o pypdf.

--- paginas/agentes_funcoes.py
+++ paginas/agentes_funcoes.py
@@ -23,7 +23,7 @@ def relator(pet_id, exame_doc_id, pdf):
     # Extraindo o texto dos pdfs
     texto = ""
     try:
-        leitor = PyPDF2.PdfReader(pdf)
+        leitor = PyPDF2.PdfReader(io.BytesIO(pdf.read()))
         for pagina in leitor.pages:
             texto_pagina = pagina.extract_text()
             if texto_pagina:
